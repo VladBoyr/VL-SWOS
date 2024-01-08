65636821c7ccf9245819125e9f4f8f9cf3287017
Vladimir Boyarskikh <vladboyr@gmail.com>
2024-01-08 13:47:59 +0500

    refactoring CountryNamesExtractor

--- VL-SWOS/SWOS.CareerMod/CountryNamesExtractor.cs
+++ VL-SWOS/SWOS.CareerMod/CountryNamesExtractor.cs
@@ -14,12 +14,12 @@ namespace SWOS.CareerMod
 
         public async Task Extract(string swosPath)
         {
-            var countryNames = new Dictionary<SwosCountry, HashSet<string>>();
-            var countrySurnames = new Dictionary<SwosCountry, HashSet<string>>();
+            var countryNames = new Dictionary<SwosCountry, Dictionary<string, int>>();
+            var countrySurnames = new Dictionary<SwosCountry, Dictionary<string, int>>();
 
             swosService.SetSwosPath(swosPath + @"\DATA\");
 
-            foreach (var swosFile in SwosFileType.AnyTeam.GetSwosFiles())
+            foreach (var swosFile in new SwosFileType[] { SwosFileType.Club, SwosFileType.NationalTeam, SwosFileType.CustomTeam }.GetSwosFiles())
             {
                 var teamsCount = await swosService.OpenSwosFile(swosFile.Value);
                 for (int teamId = 0; teamId < teamsCount; teamId++)
@@ -27,20 +27,26 @@ namespace SWOS.CareerMod
                     {
                         var playerCountry = await swosService.ReadPlayerCountry(teamId, playerId);
                         var playerName = await swosService.ReadPlayerName(teamId, playerId);
-                        var spacePositions = playerName
-                            .Select((symbol, index) => new { symbol, index })
-                            .Where(x => x.symbol == ' ')
-                            .Select(x => x.index)
-                            .ToArray();
-                        foreach (var spacePos in spacePositions)
-                        {
-                            AddName(countryNames, playerCountry, playerName[..spacePos]);
-                            AddName(countrySurnames, playerCountry, playerName[(spacePos + 1)..]);
-                        }
-                        if (spacePositions.Length == 0)
+                        var names = playerName.Split(' ');
+                        if (names.Length == 1)
                         {
                             AddName(countryNames, playerCountry, playerName);
                             AddName(countrySurnames, playerCountry, playerName);
+                        }
+                        else
+                        {
+                            foreach (var name in names.Take(names.Length - 1))
+                            {
+                                AddName(countryNames, playerCountry, name);
+                            }
+                            for (var i = 2; i < names.Length; i++)
+                            {
+                                AddName(countryNames, playerCountry, string.Join(' ', names.Take(i)));
+                            }
+                            for (var i = 1; i < names.Length; i++)
+                            {
+                                AddName(countrySurnames, playerCountry, string.Join(' ', names.Skip(i)));
+                            }
                         }
                     }
                 swosService.CloseSwosFile();
@@ -51,49 +57,26 @@ namespace SWOS.CareerMod
 
             foreach (var (country, names) in countryNames)
             {
-                await SaveNamesToFile(swosPath + $@"\NAMEFAM\{(byte)country}.n", names);                
+                await SaveNamesToFile(swosPath + $@"\NAMEFAM\{(byte)country:D3}.nam", names);                
             }
 
             foreach (var (country, surnames) in countrySurnames)
             {
-                await SaveNamesToFile(swosPath + $@"\NAMEFAM\{(byte)country}.f", surnames);
+                await SaveNamesToFile(swosPath + $@"\NAMEFAM\{(byte)country:D3}.fam", surnames);
             }
         }
 
-        private static void AddName(Dictionary<SwosCountry, HashSet<string>> dictionary, SwosCountry country, string name)
+        private static void AddName(Dictionary<SwosCountry, Dictionary<string, int>> dictionary, SwosCountry country, string name)
         {
-            if (!dictionary.ContainsKey(country))
-                dictionary.Add(country, []);
-            dictionary[country].Add(name);
+            dictionary.TryAdd(country, []);
+            dictionary[country].TryAdd(name, 0);
+            dictionary[country][name]++;
         }
 
-        private static async Task SaveNamesToFile(string fileName, HashSet<string> names)
+        private static async Task SaveNamesToFile(string fileName, Dictionary<string, int> dictionary)
         {
-            using var namesFile = new FileStream(fileName, FileMode.Create, FileAccess.Write, FileShare.None);
-
-            foreach (var name in names)
-            {
-                if (namesFile.Position != 0)
-                {
-                    var separator = new byte[1] { 0 };
-                    await namesFile.WriteAsync(separator);
-                }
-
-                var buffer = NameToByteArray(name);
-                await namesFile.WriteAsync(buffer);
-            }
-
-            namesFile.Close();
-        }
-
-        private static byte[] NameToByteArray(string name)
-        {
-            var buffer = new byte[name.Length];
-            for (var i = 0; i < buffer.Length; i++)
-            {
-                buffer[i] = (byte)name[i];
-            }
-            return buffer;
+            var names = dictionary.Select(x => $"{x.Key}={x.Value}");
+            await File.WriteAllLinesAsync(fileName, names);
         }
     }
 }
