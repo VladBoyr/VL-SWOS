d335afd11bf1585f6e93902eac8662616ae38819
Vladimir Boyarskikh <vladboyr@gmail.com>
2024-01-09 23:43:30 +0500

    SwosSkillValueExtension

--- VL-SWOS/Swos.Domain/Models/SwosSkillValue.cs
+++ VL-SWOS/Swos.Domain/Models/SwosSkillValue.cs
@@ -1,4 +1,6 @@
-﻿namespace Swos.Domain.Models;
+﻿using System.Text;
+
+namespace Swos.Domain.Models;
 
 public class SwosSkillValue
 {
@@ -14,5 +16,22 @@ public class SwosSkillValue
     public byte ToByte()
     {
         return (byte)(SkillValue + 8 * (PrimarySkill ? 1 : 0));
+    }
+}
+
+public static class SwosSkillValueExtension
+{
+    public static string ToFormatString(this Dictionary<SwosSkill, SwosSkillValue> skills)
+    {
+        var sb = new StringBuilder();
+        foreach (var (skill, skillValue) in skills.OrderBy(x => x.Key))
+        {
+            sb.Append(skill.ToChar());
+            sb.Append('=');
+            sb.Append(skillValue.SkillValue);
+            sb.Append(skillValue.PrimarySkill ? '+' : ' ');
+            sb.Append(' ');
+        }
+        return sb.ToString();
     }
 }
