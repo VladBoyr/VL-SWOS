646b20a8ae1bc67dd528fbbb9a68d1215753d896
Vladimir Boyarskikh <vladboyr@gmail.com>
2024-01-09 22:51:20 +0500

    Откатил назад

--- VL-SWOS/Common/Common.Database/IUnitOfWork.cs
+++ VL-SWOS/Common/Common.Database/IUnitOfWork.cs
@@ -2,5 +2,5 @@
 
 public interface IUnitOfWork
 {
-    Task<int> SaveChanges(CancellationToken cancellationToken = default);
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
 }
