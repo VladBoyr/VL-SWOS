8a78eef1802e0934a2b9456a39e9563bd60eceba
Vladimir Boyarskikh <vladboyr@gmail.com>
2024-06-14 10:20:35 +0500

    OptionalConverter to JSON

---
+++ VL-SWOS/Common/Common.OptionalExtension.Json/OptionalConverter.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Common.OptionalExtension.Json;
+
+public class OptionalConverter<T> : JsonConverter
+{
+    public override bool CanConvert(Type objectType)
+    {
+        return objectType == typeof(Optional<T>);
+    }
+
+    public override object? ReadJson(JsonReader reader, Type objectType, object? existingValue, JsonSerializer serializer)
+    {
+        var token = JValue.Load(reader);
+
+        if (((JValue)token).Value == null)
+        {
+            return Optional<T>.Empty;
+        }
+
+        return new Optional<T>(token.ToObject<T>()!);
+    }
+
+    public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
+    {
+        if (value == null || !((Optional<T>)value).HasValue)
+        {
+            serializer.Serialize(writer, null);
+        }
+        else
+        {
+            serializer.Serialize(writer, ((Optional<T>)value).Value);
+        }
+    }
+}
