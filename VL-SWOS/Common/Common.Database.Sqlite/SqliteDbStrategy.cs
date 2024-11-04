d71b2665b2bef3a6488de96f7fd561a00fafb13c
Vladimir Boyarskikh <vladboyr@gmail.com>
2024-11-05 01:23:12 +0500

    Common.Database.Sqlite

---
+++ VL-SWOS/Common/Common.Database.Sqlite/SqliteDbStrategy.cs
@@ -0,0 +1,80 @@
+﻿using Common.Logging;
+using Microsoft.Extensions.Logging;
+using System.Reflection;
+
+namespace Common.Database.Sqlite;
+
+public class SqliteDbStrategy
+{
+    private readonly string databaseFileName;
+    private readonly ILogger logger;
+
+    private static string AppPath => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? ".";
+    private static string DatabasePath => $"{AppPath}/Database/";
+    private static string BackupPath => $"{DatabasePath}/Backup/";
+    private static int MaxBackupCount => 100;
+
+    public SqliteDbStrategy(string databaseFileName)
+    {
+        this.databaseFileName = databaseFileName;
+        logger = LogProvider.Create(GetType());
+    }
+
+    public string ConnectionString => $"Data Source={DatabasePath}{databaseFileName};";
+
+    public Task Backup()
+    {
+        try
+        {
+            logger.LogInformation("Starting of database backup...");
+
+            if (!Directory.Exists(BackupPath))
+                Directory.CreateDirectory(BackupPath);
+
+            if (!File.Exists($"{DatabasePath}{databaseFileName}"))
+            {
+                logger.LogWarning($"Database file '{DatabasePath}{databaseFileName}' not found.");
+                return Task.CompletedTask;
+            }
+
+            var timestamp = DateTimeOffset.UtcNow.ToString("yyyy-MM-dd_HH-mm-ss_UTC_");
+            File.Copy($"{DatabasePath}{databaseFileName}", $"{BackupPath}{timestamp}{databaseFileName}");
+
+            logger.LogInformation("Database backup successfully finished.");
+
+            DeleteOldBackups();
+
+            return Task.CompletedTask;
+        }
+        catch (Exception e)
+        {
+            logger.LogError(e, "Error during database backup.");
+            throw;
+        }
+    }
+
+    private void DeleteOldBackups()
+    {
+        logger.LogInformation("Deleting old database backups...");
+
+        var files = Directory.GetFiles(BackupPath)
+            .Select(file => new FileInfo(file))
+            .OrderByDescending(file => file.LastWriteTime);
+
+        foreach (var file in files.Skip(MaxBackupCount))
+        {
+            try
+            {
+                file.Delete();
+                logger.LogInformation($"Delete old database backup '{file.Name}' complete.");
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e, $"Error during delete old database backup '{file.Name}'.");
+                throw;
+            }
+        }
+
+        logger.LogInformation("Delete old database backups finished.");
+    }
+}
--- VL-SWOS/Database/Swos.Database.Sqlite/SwosDbContextSqlite.cs
+++ VL-SWOS/Database/Swos.Database.Sqlite/SwosDbContextSqlite.cs
@@ -1,19 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
-using System.Reflection;
+﻿using Common.Database.Sqlite;
+using Microsoft.EntityFrameworkCore;
 
 namespace Swos.Database.Sqlite;
 
 public class SwosDbContextSqlite : SwosDbContext
 {
-    private static string AppPath => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? ".";
-    private static string ConnectionString => $"Data Source={DatabasePath}{DatabaseFileName};";
-    private static string DatabaseFileName => $"swos.db";
-    private static string DatabasePath => $"{AppPath}/Database/";
-    private static string BackupPath => $"{DatabasePath}/Backup/";
-    private static int MaxBackupCount => 100;
+    private static readonly SqliteDbStrategy dbStrategy = new("swos.db");
 
-    public SwosDbContextSqlite() : base(Options(ConnectionString))
+    public SwosDbContextSqlite() : base(Options(dbStrategy.ConnectionString))
     {
     }
 
@@ -23,33 +17,7 @@ public class SwosDbContextSqlite : SwosDbContext
 
     public override Task Backup()
     {
-        try
-        {
-            logger.LogInformation("Starting of database backup...");
-
-            if (!Directory.Exists(BackupPath))
-                Directory.CreateDirectory(BackupPath);
-
-            if (!File.Exists($"{DatabasePath}{DatabaseFileName}"))
-            {
-                logger.LogWarning($"Database file '{DatabasePath}{DatabaseFileName}' not found.");
-                return Task.CompletedTask;
-            }
-
-            var timestamp = DateTimeOffset.UtcNow.ToString("yyyy-MM-dd_HH-mm-ss_UTC_");
-            File.Copy($"{DatabasePath}{DatabaseFileName}", $"{BackupPath}{timestamp}{DatabaseFileName}");
-
-            logger.LogInformation("Database backup successfully finished.");
-
-            DeleteOldBackups();
-
-            return Task.CompletedTask;
-        }
-        catch (Exception e)
-        {
-            logger.LogError(e, "Error during database backup.");
-            throw;
-        }
+        return dbStrategy.Backup();
     }
 
     private static DbContextOptions Options(string connectionString)
@@ -57,30 +25,5 @@ public class SwosDbContextSqlite : SwosDbContext
         return new DbContextOptionsBuilder()
             .UseSqlite(connectionString)
             .Options;
-    }
-
-    private void DeleteOldBackups()
-    {
-        logger.LogInformation("Deleting old database backups...");
-
-        var files = Directory.GetFiles(BackupPath)
-            .Select(file => new FileInfo(file))
-            .OrderByDescending(file => file.LastWriteTime);
-
-        foreach (var file in files.Skip(MaxBackupCount))
-        {
-            try
-            {
-                file.Delete();
-                logger.LogInformation($"Delete old database backup '{file.Name}' complete.");
-            }
-            catch (Exception e)
-            {
-                logger.LogError(e, $"Error during delete old database backup '{file.Name}'.");
-                throw;
-            }
-        }
-
-        logger.LogInformation("Delete old database backups finished.");
     }
 }
